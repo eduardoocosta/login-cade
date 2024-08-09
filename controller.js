f1ef45b6dc760c67e551bf0848f4ccad7e37f99e
EDUARDO COSTA <e.costa5@alunoescolassociais.org.br>
2024-08-09 08:03:44 -0300

    insere o README

---
+++ controller.js
@@ -0,0 +1,48 @@
+// função para validação de acesso
+function acessar() {
+    let loginEmail = document.getElementById("loginEmail").value;
+    let loginSenha = document.getElementById("loginSenha").value;
+    if (!loginEmail || !loginSenha) {
+      alert("Favor preencher todos os campos");
+    } else {
+      // alert("Campos preenchidos com sucesso");
+      window.location.href = "cadastro.html";
+    }
+  }
+   
+  // função para armazenamento de nomes em array
+   
+  var dadosLista = [];
+  function salvarUser() {
+    let nomeUser = document.getElementById("nomeUser").value;
+    if (nomeUser) {
+      dadosLista.push(nomeUser);
+      //console.log(dadosListas);
+      criaLista();
+      document.getElementById('nomeUser').value = "";
+    } else {
+      alert("Favor informe um nome para cadastro");
+    }
+  }
+   
+// Pegar o nome do array para tranferir nos usuários
+function criaLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    for(let i=0;i <= (dadosLista.length - 1);i++){
+      tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>excluir</button></td></tr>";
+      document.getElementById('tabela'). innerHTML = tabela;
+    }
+}
+
+// EDITAR O NOMES DE LISTA
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista [(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)],1);
+}
+
+// EXCLUIR NOME DE LISTA
+function excluir(i){
+    dadosLista.splice((i - 1), 1);
+    document.getElementById('tabela').deleteRow(i);
+
+}
