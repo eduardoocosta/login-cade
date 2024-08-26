09b53449ad170bc1fe8adfd520109a8b14fc341d
EDUARDO COSTA <e.costa5@alunoescolassociais.org.br>
2024-08-26 09:20:45 -0300

    insere o README

--- controller.js
+++ controller.js
@@ -1,6 +1,5 @@
 // Validar o acesso do login
 function acessar() {
-    let loginEmail = document.getElementById("loginEmail").value;
     let loginSenha = document.getElementById("loginSenha").value;
     if (!loginEmail || !loginSenha) {
       alert("Favor preencher todos os campos");
